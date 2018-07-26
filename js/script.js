8fb454c3a73098015943207a5d3bc10691905da7
AJAEGBU SHEDRACK IWUCHUKWU <shedrack3@gmail.com>
2018-07-26 07:23:13 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,27 @@
+//declaration section
+const open_menu = document.getElementById("open_menu");
+const close_menu = document.querySelectorAll("header ul a");
+const navMenu = document.getElementById("menu");
+const closeNav = document.getElementById("close_menu");
+
+//opens the navbar when a user clicks on the navbar icon
+open_menu.addEventListener("click", () => {
+  if (navMenu.classList != "open") {
+    navMenu.classList.add("open");
+
+    open_menu.classList.add("open-menu");
+    closeNav.classList.remove("close-menu");
+  }
+}, false);
+
+//close the navigation bar when a user clicks on close icon or on any of the menu
+for (let i = 0; i <= close_menu.length; i++) {
+  close_menu[i].addEventListener("click", () => {
+    if (navMenu.classList.contains("open")) {
+      navMenu.classList.remove("open");
+
+      open_menu.classList.remove("open-menu");
+      closeNav.classList.add("close-menu");
+    }
+  }, false);
+}
